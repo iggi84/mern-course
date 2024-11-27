f06135c3e237c3e3281bc3e81ee88195daaa5296
Igor Vojinovic <igor.vojinovic>
2024-11-28 00:44:23 +1100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://opto.website", // Allow requests from your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow cookies or credentials if needed
+  })
+);
+
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production") {
