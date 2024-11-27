3463bc17e00e74278c7a16a8cce92c57f1dcddb2
Igor Vojinovic <igor.vojinovic>
2024-11-28 00:20:32 +1100

    Restored original store.

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,15 +1,13 @@
 import { create } from "zustand";
-const API_URL = import.meta.env.VITE_API_URL;
+
 export const useProductStore = create((set) => ({
-  products: [],
+	products: [],
 	setProducts: (products) => set({ products }),
 	createProduct: async (newProduct) => {
 		if (!newProduct.name || !newProduct.image || !newProduct.price) {
 			return { success: false, message: "Please fill in all fields." };
 		}
-    console.log("FetchingData from products");
-    console.log(API_URL);
-		const res = await fetch(`${API_URL}/products`, {
+		const res = await fetch("/api/products", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -21,12 +19,12 @@ export const useProductStore = create((set) => ({
 		return { success: true, message: "Product created successfully" };
 	},
 	fetchProducts: async () => {
-		const res = await fetch('${API_URL}/products');
+		const res = await fetch("/api/products");
 		const data = await res.json();
 		set({ products: data.data });
 	},
 	deleteProduct: async (pid) => {
-		const res = await fetch(`${API_URL}/products/${pid}`, {
+		const res = await fetch(`/api/products/${pid}`, {
 			method: "DELETE",
 		});
 		const data = await res.json();
@@ -37,7 +35,7 @@ export const useProductStore = create((set) => ({
 		return { success: true, message: data.message };
 	},
 	updateProduct: async (pid, updatedProduct) => {
-		const res = await fetch(`${API_URL}/products/${pid}`, {
+		const res = await fetch(`/api/products/${pid}`, {
 			method: "PUT",
 			headers: {
 				"Content-Type": "application/json",
